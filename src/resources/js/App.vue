8c64fceed22eb5cfc666d3940c4dcb0d124e2995
柳澤風輝 <yanagisawa@yanagisawakazeakiranoMacBook-Pro.local>
2021-11-18 06:22:27 +0900

    vuetify導入

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -1,9 +1,28 @@
 <template>
-    <div>
-        <main>
-            <div class="container">
-                <RouterView />
-            </div>
-        </main>
-    </div>
-</template>;
+    <v-app>
+
+        <Header />
+
+        <v-main>
+            <HelloWorld />
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+
+export default {
+    name: "App",
+
+    components: {
+        HelloWorld,
+        Header
+    },
+
+    data: () => ({
+        //
+    }),
+};
+</script>
--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -32,14 +32,21 @@
 // });
 
 import Vue from "vue";
-// ルーティングの定義をインポートする
 import router from "./router";
-// ルートコンポーネントをインポートする
 import App from "./App.vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "@mdi/font/css/materialdesignicons.css";     //<v-icon>アイコンを使えるように
+
+Vue.use(Vuetify);
 
 new Vue({
     el: "#app",
+    vuetify: new Vuetify(),
     router, // ルーティングの定義を読み込む
     components: { App }, // ルートコンポーネントの使用を宣言する
     template: "<App />", // ルートコンポーネントを描画する
+    icons: {
+        iconfont: "mdi",
+    },
 });
